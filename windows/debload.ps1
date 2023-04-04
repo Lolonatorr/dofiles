2dd3a697f585bb849636ae104adf4b05247d7ed0
lolonatorr <enst18.12@gmail.com>
2023-04-04 13:59:07 +0200

    refined

--- windows/debload.ps1
+++ windows/debload.ps1
@@ -1,8 +1,8 @@
 Write-host "windows uninstaller bruh run as admin"
 write-host $packages
 $choice = Read-Host "do you want to uninstall all of the following packages:(Type yes)"
-$packages = @('Clipchamp','Maps','Media Player','Phone Link','camera','Clock','Cortana','Feedback Hub','Get Help','Mail and Calendar','Microsoft Teams','Microsoft To Do','News','Paint','People','Quick Assist','Power Automate','Solitaire & Casual Games','Spotify Music','Sticky Notes','Tips','Voice Recorder','Weather','Xbox Live')
-$appx_packages = @('windowsmaps','People','WindowsSoundRecorder','WebExperience','Teams')
+$packages = @('Clipchamp','Maps','Media Player','Phone Link','camera','Clock','Cortana','Mail and Calendar','Microsoft Teams','Microsoft To Do','News','Paint','People','Quick Assist','Power Automate','Solitaire & Casual Games','Spotify Music','Sticky Notes','Tips','Voice Recorder','Weather','Xbox Live')
+$appx_packages = @('windowsmaps','People','WindowsSoundRecorder','WebExperience','Teams','Feedback','Help','Assist','Phone')
 foreach ($i in $packages){
     write-host $i
 }
