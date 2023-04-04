fd0f48054f8c204be6454bfe43865a764eb68b02
phonkd <enst18.12@gmail.com>
2023-04-04 14:44:01 +0200

    nun

--- scripts/archinstaller.sh
+++ scripts/archinstaller.sh
@@ -1,3 +1,40 @@
-echo "installing bloat"
-sudo pacman -S -y discord alacritty
-yay -S cava visual-studio-code-bin spotify
+echo "Free System Bloater v0.1"
+#sudo pacman -S -y discord alacritty
+#yay -S cava visual-studio-code-bin snapd
+#sudo systemctl start snapd
+#sudo snap install spotify
+pacman_pkgs=(
+discord
+alacritty    
+)
+pacman_cmd="sudo pacman -S "
+
+aur_pkgs=(
+cava
+visual-studio-code-bin
+kwin-bismuth
+snapd
+)
+yay_cmd="yay -S "
+
+snap_pkgs=(
+spotify    
+)
+snap_cmd="sudo snap install"
+
+echo "You are about to install the following applications:"
+echo "-----------Pacman-----------"
+for i in "${pacman_pkgs[@]}"
+do
+    echo "$i"
+done 
+echo "-----------AUR-----------"
+for i in "${aur_pkgs[@]}"
+do
+    echo "$i"
+done 
+echo "-----------SNAP-----------"
+for i in "${snap_pkgs[@]}"
+do
+    echo "$i"
+done 
